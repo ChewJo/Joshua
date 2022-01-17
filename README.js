f7c6a85d6bc87b6372fb2724143ad6ebaa2239f8
Joshua Chewings <83604514+ChewJo@users.noreply.github.com>
2022-01-17 17:38:26 +0000

    Create README.js

---
+++ README.js
@@ -0,0 +1,12 @@
+const name = 'Joshua';
+
+const skills = 
+{
+  languages  : [ Python, HTML, Javascript ],
+  
+  ide        : [  Visual Studio Code ],
+  
+  frameworks : [ 'Discord.JS', 'ThreeJS' ],
+  
+  
+}
